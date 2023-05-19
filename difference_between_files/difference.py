6a9b9e445657c64f1b1cefc94d45692a1b51f4fe
timur <timurka.zavyalov@mail.ru>
2023-05-19 09:30:44 +0300

    add: added new difference

--- difference_between_files/difference.py
+++ difference_between_files/difference.py
@@ -63,8 +63,66 @@ def filter_diffs(diffs):
         diffs[i[0]] = i[2]
     return diffs
 
+def sequence_matcher(table,text1,text2,count_error,number):
+    cells = table.add_row().cells
+    cells[0].width = Inches(0.6)
+    cells[1].width = Inches(3)
+    cells[2].width = Inches(3)
+    cells[0].text = number
 
-def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
+    left_diff_count = 0
+    right_diff_count = 0
+    sequence = difflib.SequenceMatcher(a=text1.lower(), b=text2.lower(), autojunk=False)
+
+    paragraph1 = cells[1].paragraphs[0]
+    paragraph2 = cells[2].paragraphs[0]
+
+    logger.info('Write document')
+    for op, i1, i2, j1, j2 in sequence.get_opcodes():
+        run1 = paragraph1.add_run(text1[i1:i2])
+        run2 = paragraph2.add_run(text2[j1:j2])
+        if op in ("delete", "insert"):
+            if text1[i1:i2] not in skips:
+                run1.font.highlight_color = WD_COLOR_INDEX.YELLOW
+                left_diff_count += i2 - i1
+            if text2[j1:j2] not in skips:
+                run2.font.highlight_color = WD_COLOR_INDEX.YELLOW
+                right_diff_count += j2 - j1
+        if op == "replace":
+            if (text1[i1:i2], text2[j1:j2]) not in replacements and (
+                    text2[j1:j2],
+                    text1[i1:i2],
+            ) not in replacements:
+                run1.font.highlight_color = WD_COLOR_INDEX.YELLOW
+                run2.font.highlight_color = WD_COLOR_INDEX.YELLOW
+                left_diff_count += i2 - i1
+                right_diff_count += j2 - j1
+
+    if left_diff_count <= count_error and right_diff_count <= count_error:
+        table._tbl.remove(table.rows[-1]._tr)
+
+
+def add_paragraph(table,text1,text2,number):
+    cells = table.add_row().cells
+    cells[0].width = Inches(0.6)
+    cells[1].width = Inches(3)
+    cells[2].width = Inches(3)
+    cells[0].text = number
+    paragraph1 = cells[1].paragraphs[0]
+    paragraph2 = cells[2].paragraphs[0]
+    paragraph1.add_run(text1)
+    paragraph2.add_run(text2)
+    return
+
+def flagg_append(flag_text,text1,text2,number,diff):
+    flag_text[1].append(text1)
+    flag_text[2].append(text2)
+    flag_text[3].append(diff.is_different)
+    flag_text[0] = number.strip(' ✓ ')
+
+
+
+def save_disagreement(file1: str, file2: str, count_error: int, flag: bool = False) -> io.BytesIO:
     logger.info('Create document')
     result = Document()
     result.add_heading("Протокол разногласий")
@@ -82,6 +140,7 @@ def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
     list1, list2 = list_from_string(file1), list_from_string(file2)
     diffs = list(get_diff(list1, list2))
     number_flag = ''
+    flag_text = [0, [], [],[]]
     for diff in diffs:
         if not diff.last_known_number and not diff.current_number:
             number = ''
@@ -89,55 +148,73 @@ def save_disagreement(file1: str, file2: str, count_error: int) -> io.BytesIO:
             number = diff.last_known_number + ' ✓ ' if not diff.current_number else diff.current_number
         text1, text2 = [re.sub(r"(?:^(\.?,?\d{0,2}){0,4} ?|\.?,?$)", "", text).strip() for text in
                         [diff.first_column, diff.second_column]]
-        if not diff.is_different and count_error == 0:
-            cells = table.add_row().cells
-            cells[0].width = Inches(0.6)
-            cells[1].width = Inches(3)
-            cells[2].width = Inches(3)
-            cells[0].text = number
-            paragraph1 = cells[1].paragraphs[0]
-            paragraph2 = cells[2].paragraphs[0]
-            paragraph1.add_run(text1)
-            paragraph2.add_run(text2)
+        if not diff.is_different and count_error == 0 and not flag:
+            add_paragraph(table,text1,text2,number)
             continue
-        if not diff.is_different:
+        if diff.is_different and count_error == 0 and not flag:
+            sequence_matcher(table,text1,text2,count_error,number)
             continue
-        cells = table.add_row().cells
-        cells[0].width = Inches(0.6)
-        cells[1].width = Inches(3)
-        cells[2].width = Inches(3)
-        cells[0].text = number
 
-        left_diff_count = 0
-        right_diff_count = 0
-        sequence = difflib.SequenceMatcher(a=text1.lower(), b=text2.lower(), autojunk=False)
+        if count_error == 0 and flag:
+            if diff.current_number == '':
+                flagg_append(flag_text,text1,text2,number,diff)
 
-        paragraph1 = cells[1].paragraphs[0]
-        paragraph2 = cells[2].paragraphs[0]
+                continue
+            else:
+                if len(flag_text[1])>1 :
+                    if  any([i for i in flag_text[3]]):
+                        sequence_matcher(table, ''.join(flag_text[1]), ''.join(flag_text[2]), count_error, flag_text[0])
+                        flag_text = [0, [], [],[]]
+                        flagg_append(flag_text, text1, text2, number, diff)
+                    else:
+                        add_paragraph(table,''.join(flag_text[1]),''.join(flag_text[2]),flag_text[0])
+                        flag_text = [0, [], [], []]
+                        flagg_append(flag_text, text1, text2, number, diff)
+                else:
+                    if len(flag_text[1])==1 and not flag_text[3][0]:
+                        add_paragraph(table, ''.join(flag_text[1]), ''.join(flag_text[2]), flag_text[0])
+                        flag_text = [0, [], [],[]]
+                        flagg_append(flag_text, text1, text2, number, diff)
+                    else:
+                        if len(flag_text[1]) == 1 and flag_text[3][0]:
+                            sequence_matcher(table, ''.join(flag_text[1]), ''.join(flag_text[2]), count_error,
+                                             flag_text[0])
+                            flag_text = [0, [], [], []]
+                            flagg_append(flag_text, text1, text2, number, diff)
 
-        logger.info('Write document')
-        for op, i1, i2, j1, j2 in sequence.get_opcodes():
-            run1 = paragraph1.add_run(text1[i1:i2])
-            run2 = paragraph2.add_run(text2[j1:j2])
-            if op in ("delete", "insert"):
-                if text1[i1:i2] not in skips:
-                    run1.font.highlight_color = WD_COLOR_INDEX.YELLOW
-                    left_diff_count += i2 - i1
-                if text2[j1:j2] not in skips:
-                    run2.font.highlight_color = WD_COLOR_INDEX.YELLOW
-                    right_diff_count += j2 - j1
-            if op == "replace":
-                if (text1[i1:i2], text2[j1:j2]) not in replacements and (
-                        text2[j1:j2],
-                        text1[i1:i2],
-                ) not in replacements:
-                    run1.font.highlight_color = WD_COLOR_INDEX.YELLOW
-                    run2.font.highlight_color = WD_COLOR_INDEX.YELLOW
-                    left_diff_count += i2 - i1
-                    right_diff_count += j2 - j1
+                        else:
+                            flagg_append(flag_text, text1, text2, number, diff)
 
-        if left_diff_count <= count_error and right_diff_count <= count_error:
-            table._tbl.remove(table.rows[-1]._tr)
+                continue
+        if count_error>0 and not flag:
+            sequence_matcher(table,text1,text2,count_error,number)
+            continue
+
+        if count_error>0 and flag:
+                if diff.current_number == '':
+                    flagg_append(flag_text, text1, text2, number, diff)
+                    continue
+                if len(flag_text[1])>1 :
+                    if  all([not i for i in flag_text[3]]):
+                        flag_text = [0, [], [], []]
+                        continue
+                    else:
+                        sequence_matcher(table,''.join(flag_text[1]),''.join(flag_text[2]),count_error,flag_text[0])
+                        flag_text = [0, [], [],[]]
+                        flagg_append(flag_text, text1, text2, number, diff)
+                else:
+                    if len(flag_text[1])==1 and flag_text[3][0]:
+                        sequence_matcher(table,flag_text[1][0],flag_text[2][0],count_error,flag_text[0])
+                        flag_text = [0, [], [], []]
+                        flagg_append(flag_text, text1, text2, number, diff)
+                    else:
+                        flag_text = [0, [], [], []]
+                        flagg_append(flag_text, text1, text2, number, diff)
+
+
+
+
+
 
     file_stream = io.BytesIO()
     result.save(file_stream)
